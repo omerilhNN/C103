5eee40858c591c172178412a8ded3596ef218f90
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2024-04-04 11:10:58 +0300

    update main.c

--- main.c
+++ main.c
@@ -14,22 +14,33 @@ void* routine(void* arg) {
     *(int*)arg = sum;
     return arg;
 }
+void* routine1(void* arg) {
+    pthread_t tid = *(pthread_t*)arg;
+    printf("Hello: Routine thread ID: %ld\n", tid);
+    pthread_exit(NULL);
+}
 
 int main(int argc, char* argv[]) {
-    pthread_t th[2];
+    pthread_t thSum[2], thPrint[2];
     int i;
     for (i = 0; i < 2; i++) {
         int* a = malloc(sizeof(int));
         *a = i * 5;
-        if (pthread_create(&th[i], NULL, &routine, a) != 0) {
-            perror("Failed to create thread");
+        if (pthread_create(&thSum[i], NULL, &routine, a) != 0) {
+            perror("Failed to create threadSum\n");
+        }
+        if (pthread_create(&thPrint[i], NULL, routine1, &thSum[i]) != 0) {
+            perror("Failed to create ROUTINE1\n");
         }
     }
     int globalSum = 0;
     for (i = 0; i < 2; i++) {
         int* r;
-        if (pthread_join(th[i], (void**)&r) != 0) {
-            perror("Failed to join thread");
+        if (pthread_join(thSum[i], (void**)&r) != 0) {
+            perror("Failed to join thread\n");
+        }
+        if (pthread_join(thPrint[i], NULL)){
+            perror("Failed to join print thread\n");
         }
         globalSum += *r;
         free(r);
