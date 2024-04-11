677b6db9f5fa404f07388801032913d4f4599df0
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2024-04-11 12:12:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,19 @@
 #include <pthread.h>
 
 int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+pthread_mutex_t mutex;
+
 
 void* routine(void* arg) {
     int index = *(int*)arg;
+    pthread_mutex_lock(&mutex);
     int sum = 0;
     for (int j = 0; j < 5; j++) {
         sum += primes[index + j];
     }
     printf("Local sum: %d\n", sum);
     *(int*)arg = sum;
+    pthread_mutex_unlock(&mutex);
     return arg;
 }
 void* routine1(void* arg) {
@@ -23,13 +27,15 @@ void* routine1(void* arg) {
 int main(int argc, char* argv[]) {
     pthread_t thSum[2], thPrint[2];
     int i;
+    pthread_mutex_init(&mutex,NULL);
+
     for (i = 0; i < 2; i++) {
         int* a = malloc(sizeof(int));
-        *a = i * 5;
+        *a = i * 5;  
         if (pthread_create(&thSum[i], NULL, &routine, a) != 0) {
             perror("Failed to create threadSum\n");
         }
-        if (pthread_create(&thPrint[i], NULL, routine1, &thSum[i]) != 0) {
+        if (pthread_create(&thPrint[i], NULL, &routine1, &thSum[i]) != 0) {
             perror("Failed to create ROUTINE1\n");
         }
     }
@@ -45,6 +51,8 @@ int main(int argc, char* argv[]) {
         globalSum += *r;
         free(r);
     }
+
     printf("Global sum: %d\n", globalSum);
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
